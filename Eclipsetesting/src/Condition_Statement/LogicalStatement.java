9b80689c2707cb8d7d797cc756d3a6c2895995ac
Aparna Eshwar <Aparna Eshwar@192.168.1.35>
2022-12-20 12:47:08 +0530

    20/12/2022

---
+++ Eclipsetesting/src/Condition_Statement/LogicalStatement.java
@@ -0,0 +1,21 @@
+package Condition_Statement;
+
+public class LogicalStatement {
+public static void main (String[]args) { 
+	int a = 40;
+	int b = 20;
+	if(a!=1)
+	{
+		System.out.println("even number");
+				
+	
+}
+
+
+}
+}
+			
+
+
+
+
